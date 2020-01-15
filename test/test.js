475c35336d35ce7fb4eb11ee48f3dcf56390d4b8
Yagi Sumiya <goatship4@yahoo.co.jp>
2020-01-16 05:58:57 +0900

    Add test

---
+++ test/test.js
@@ -0,0 +1,50 @@
+const assert = require('assert')
+const { OutputDebugString, monitor } = require('../lib')
+
+function sleep(msec) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, msec)
+  })
+}
+
+async function test() {
+  assert.strictEqual(monitor.stop(), false)
+
+  const outputs = []
+  const r1 = monitor.start(({ message }) => {
+    outputs.push(message)
+  })
+
+  assert.strictEqual(r1.ok, true)
+  assert.strictEqual(r1.error, undefined)
+
+  OutputDebugString('test1')
+  OutputDebugString('test2')
+
+  await sleep(300)
+
+  assert.deepStrictEqual(outputs, ['test1', 'test2'])
+
+  const r2 = monitor.start(() => {})
+
+  assert.strictEqual(r2.ok, false)
+  assert.strictEqual(typeof r2.error, 'object')
+  assert.strictEqual(r2.error.name, 'AlreadyStartingError')
+
+  assert.strictEqual(monitor.stop(), true)
+}
+
+async function main() {
+  const err = await test().catch((err) => err)
+  monitor.stop()
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  } else {
+    console.log('test passed')
+  }
+}
+
+main()
