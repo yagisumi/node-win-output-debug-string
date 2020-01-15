a5581ab857415318ca441d0f09714c8dcfbba520
Yagi Sumiya <goatship4@yahoo.co.jp>
2020-01-16 07:08:35 +0900

    Update type

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,18 @@
-type OK = { ok: true, error: undefined }
-type ERR = { ok: false, error: { name: string, message: string }}
-interface Monitor {
-  start(callback: (info: { pid: number, message: string }) => void): OK | ERR
+declare type OK = {
+  ok: true
+  error: undefined
+}
+declare type ERR = {
+  ok: false
+  error: {
+    name: string
+    message: string
+  }
+}
+interface monitor {
+  start(callback: (info: { pid: number; message: string }) => void): OK | ERR
   stop(): boolean
 }
-export declare const monitor: Monitor
+
+export declare const monitor: monitor
 export declare function OutputDebugString(message: string): void
