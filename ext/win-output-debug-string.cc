e14938aed2768ba9a3d0ef1b4f4ca6b89d290301
Yagi Sumiya <goatship4@yahoo.co.jp>
2020-10-15 23:41:31 +0900

    Prevent keeping object references
    It causes a crash when the Electron exits.

--- ext/win-output-debug-string.cc
+++ ext/win-output-debug-string.cc
@@ -33,53 +33,37 @@ enum ErrorCode {
   EventAlreadyExistsError,
 };
 
-static ObjectReference monitor;
-
-class Monitor : public ObjectWrap<Monitor> {
-  public:
-    static Object Init(Napi::Env env, Object exports);
-    static FunctionReference constructor;
-    Monitor(const CallbackInfo &info);
-    Napi::Value start(const CallbackInfo &info);
-    Napi::Value stop(const CallbackInfo &info);
-
-  private:
-    bool running = false;
-    bool securityInitialized = false;
-    HANDLE file = NULL;
-    OdsBuffer* buf = nullptr;
-    SECURITY_ATTRIBUTES secAttr;
-    SECURITY_DESCRIPTOR secDesc;
-    HANDLE BUFFER_READY = NULL;
-    HANDLE DATA_READY = NULL;
-    ThreadSafeFunction tsfn;
-    std::thread nativeThread;
-    bool initializeSecurity();
-    ErrorCode initializeResources();
-    void destroyResources();
-    Napi::Value error(const CallbackInfo &info, ErrorCode errCode);
-    Napi::Value ok(const CallbackInfo &info);
+struct MonitorContext {
+  bool running = false;
+  bool securityInitialized = false;
+  HANDLE file = NULL;
+  OdsBuffer* buf = nullptr;
+  SECURITY_ATTRIBUTES secAttr;
+  SECURITY_DESCRIPTOR secDesc;
+  HANDLE BUFFER_READY = NULL;
+  HANDLE DATA_READY = NULL;
+  ThreadSafeFunction tsfn;
+  std::thread nativeThread;
 };
 
-FunctionReference Monitor::constructor;
 
-Monitor::Monitor(const Napi::CallbackInfo &info) : Napi::ObjectWrap<Monitor>(info) {}
+static struct MonitorContext context;
 
-bool Monitor::initializeSecurity() {
-  secAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
-  secAttr.bInheritHandle = TRUE;
-  secAttr.lpSecurityDescriptor = &secDesc;
+bool initializeSecurity() {
+  context.secAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+  context.secAttr.bInheritHandle = TRUE;
+  context.secAttr.lpSecurityDescriptor = &context.secDesc;
 
-  if (!InitializeSecurityDescriptor(&secDesc, SECURITY_DESCRIPTOR_REVISION))
+  if (!InitializeSecurityDescriptor(&context.secDesc, SECURITY_DESCRIPTOR_REVISION))
     return false;
 
-  if (!SetSecurityDescriptorDacl(&secDesc, TRUE, (PACL)NULL, FALSE))
+  if (!SetSecurityDescriptorDacl(&context.secDesc, TRUE, (PACL)NULL, FALSE))
     return false;
 
   return true;
 }
 
-Value Monitor::ok(const CallbackInfo &info) {
+Value ok(const CallbackInfo &info) {
   auto env = info.Env();
   auto obj = Object::New(env);
   obj["ok"] = Boolean::New(env, true);
@@ -87,7 +71,7 @@ Value Monitor::ok(const CallbackInfo &info) {
   return obj;
 }
 
-Value Monitor::error(const CallbackInfo &info, ErrorCode errCode) {
+Value error(const CallbackInfo &info, ErrorCode errCode) {
   auto env = info.Env();
   auto obj = Object::New(env);
 
@@ -127,8 +111,61 @@ Value Monitor::error(const CallbackInfo &info, ErrorCode errCode) {
   return obj;
 }
 
-Value Monitor::start(const CallbackInfo &info) {
-  if (running) {
+
+ErrorCode initializeResources() {
+  context.file = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(*context.buf), TEXT("DBWIN_BUFFER"));
+  if (context.file == INVALID_HANDLE_VALUE) {
+    return ResourcesInitializationError;
+  }
+
+  context.buf = static_cast<OdsBuffer *>(MapViewOfFile(context.file, SECTION_MAP_READ, 0, 0, 0));
+  if (context.buf == nullptr) {
+    return ResourcesInitializationError;
+  }
+
+  context.BUFFER_READY = CreateEvent(&context.secAttr, FALSE, FALSE, TEXT("DBWIN_BUFFER_READY"));
+  if (context.BUFFER_READY == NULL) {
+    return ResourcesInitializationError;
+  }
+  if (GetLastError() == ERROR_ALREADY_EXISTS) {
+    return EventAlreadyExistsError;
+  }
+
+  context.DATA_READY = CreateEvent(&context.secAttr, FALSE, FALSE, TEXT("DBWIN_DATA_READY"));
+  if (context.DATA_READY == NULL) {
+    return ResourcesInitializationError;
+  }
+  if (GetLastError() == ERROR_ALREADY_EXISTS) {
+    return EventAlreadyExistsError;
+  }
+
+  return OK;
+}
+
+void destroyResources() {
+  if (context.DATA_READY != NULL) {
+    CloseHandle(context.DATA_READY);
+    context.DATA_READY = NULL;
+  }
+
+  if (context.BUFFER_READY != NULL) {
+    CloseHandle(context.BUFFER_READY);
+    context.BUFFER_READY = NULL;
+  }
+
+  if (context.buf != nullptr) {
+    UnmapViewOfFile(context.buf);
+    context.buf = nullptr;
+  }
+
+  if (context.file != INVALID_HANDLE_VALUE && context.file != 0) {
+    CloseHandle(context.file);
+    context.file = 0;
+  }
+}
+
+Value start(const CallbackInfo &info) {
+  if (context.running) {
     return error(info, AlreadyStartingError);
   }
 
@@ -137,9 +174,9 @@ Value Monitor::start(const CallbackInfo &info) {
   }
   Function func = info[0].As<Function>();
 
-  if (!securityInitialized) {
-    securityInitialized = initializeSecurity();
-    if (!securityInitialized) {
+  if (!context.securityInitialized) {
+    context.securityInitialized = initializeSecurity();
+    if (!context.securityInitialized) {
       return error(info, SecurityInitializationError);
     }
   }
@@ -150,19 +187,19 @@ Value Monitor::start(const CallbackInfo &info) {
     return error(info, err);
   }
 
-  tsfn = ThreadSafeFunction::New(
+  context.tsfn = ThreadSafeFunction::New(
     info.Env(),
     func,
     "monitor",
     0,
     1,
-    [this](Napi::Env) {
-      this->nativeThread.join();
+    [](Napi::Env) {
+      context.nativeThread.join();
     }
   );
 
-  running = true;
-  nativeThread = std::thread([this]() {
+  context.running = true;
+  context.nativeThread = std::thread([]() {
     auto callback = []( Napi::Env env, Function jsCallback, OdsInfo* info) {
       auto obj = Object::New(env);
       obj["pid"] = Number::New(env, info->pid);
@@ -173,111 +210,39 @@ Value Monitor::start(const CallbackInfo &info) {
     };
 
     DWORD r;
-    SetEvent(this->BUFFER_READY);
+    SetEvent(context.BUFFER_READY);
 
-    while (this->running) {
-      r = WaitForSingleObject(this->DATA_READY, INFINITE);
+    while (context.running) {
+      r = WaitForSingleObject(context.DATA_READY, INFINITE);
       if (r != WAIT_OBJECT_0) {
         break;
       }
-      if (!this->running) {
+      if (!context.running) {
         break;
       }
 
-      OdsInfo* info = new OdsInfo(this->buf);
-      napi_status status = this->tsfn.NonBlockingCall(info, callback);
+      OdsInfo* info = new OdsInfo(context.buf);
+      napi_status status = context.tsfn.NonBlockingCall(info, callback);
       if (status != napi_ok) {
         delete info;
       }
-      SetEvent(BUFFER_READY);
+      SetEvent(context.BUFFER_READY);
     }
-    tsfn.Release();
-    this->destroyResources();
-    this->running = false;
+    context.tsfn.Release();
+    destroyResources();
+    context.running = false;
   });
 
   return ok(info);
 }
 
-ErrorCode Monitor::initializeResources() {
-  file = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(*buf), TEXT("DBWIN_BUFFER"));
-  if (file == INVALID_HANDLE_VALUE) {
-    return ResourcesInitializationError;
-  }
-
-  buf = static_cast<OdsBuffer *>(MapViewOfFile(file, SECTION_MAP_READ, 0, 0, 0));
-  if (buf == nullptr) {
-    return ResourcesInitializationError;
-  }
-
-  BUFFER_READY = CreateEvent(&secAttr, FALSE, FALSE, TEXT("DBWIN_BUFFER_READY"));
-  if (BUFFER_READY == NULL) {
-    return ResourcesInitializationError;
-  }
-  if (GetLastError() == ERROR_ALREADY_EXISTS) {
-    return EventAlreadyExistsError;
-  }
-
-  DATA_READY = CreateEvent(&secAttr, FALSE, FALSE, TEXT("DBWIN_DATA_READY"));
-  if (DATA_READY == NULL) {
-    return ResourcesInitializationError;
-  }
-  if (GetLastError() == ERROR_ALREADY_EXISTS) {
-    return EventAlreadyExistsError;
-  }
-
-  return OK;
-}
-
-void Monitor::destroyResources() {
-  if (DATA_READY != NULL) {
-    CloseHandle(DATA_READY);
-    DATA_READY = NULL;
-  }
-
-  if (BUFFER_READY != NULL) {
-    CloseHandle(BUFFER_READY);
-    BUFFER_READY = NULL;
-  }
-
-  if (buf != nullptr) {
-    UnmapViewOfFile(buf);
-    buf = nullptr;
-  }
-
-  if (file != INVALID_HANDLE_VALUE && file != 0) {
-    CloseHandle(file);
-    file = 0;
-  }
-}
-
-Value Monitor::stop(const CallbackInfo &info) {
-  running = false;
-  if (DATA_READY) {
-    SetEvent(DATA_READY);
+Value stop(const CallbackInfo &info) {
+  context.running = false;
+  if (context.DATA_READY) {
+    SetEvent(context.DATA_READY);
     return Boolean::New(info.Env(), true);
   }
   return Boolean::New(info.Env(), false);
-}
-
-
-Object Monitor::Init(Napi::Env env, Object exports) {
-  Function func = DefineClass(env, "Monitor", {
-    InstanceMethod("start", &Monitor::start),
-    InstanceMethod("stop", &Monitor::stop),
-  });
-
-  constructor = Persistent(func);
-  constructor.SuppressDestruct();
-
-  if (monitor == nullptr) {
-    auto instance = constructor.New({});
-    monitor = Persistent(instance);
-    monitor.SuppressDestruct();
-  }
-
-  exports["monitor"] = monitor.Value();
-  return exports;
 }
 
 Value outputDebugString(const CallbackInfo& info) {
@@ -291,7 +256,11 @@ Value outputDebugString(const CallbackInfo& info) {
 
 Object Init(Env env, Object exports) {
   exports["OutputDebugString"] = Function::New(env, outputDebugString);
-  Monitor::Init(env, exports);
+
+  auto monitor = Object::New(env);
+  monitor["start"] = Function::New(env, start);
+  monitor["stop"] = Function::New(env, stop);
+  exports["monitor"] = monitor;
   return exports;
 }
 
